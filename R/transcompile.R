af9ce075e91e94be00c9f5e7dfe2850625e86280
David B. Dahl <dahl@stat.byu.edu>
2018-07-11 15:23:42 -0600

    More transcompilation

--- R/transcompile.R
+++ R/transcompile.R
@@ -1,7 +1,7 @@
 rscalaTranscompileHeader <- c("import org.ddahl.commonsmath.Transcompile._")
 
 rscalaTranscompileSubstitute <- function(x) {
-  if ( x %in% c("gamma","lgamma","beta","lbeta","factorial","lfactorial","choose","lchoose","stirlingS2","dnorm","pnorm","qnorm","rnorm") ) paste0("_",x)
+  if ( x %in% c("gamma","lgamma","beta","lbeta","factorial","lfactorial","choose","lchoose","stirlingS2","sample","dnorm","pnorm","qnorm","rnorm") ) paste0("_",x)
   else NULL
 }
 
