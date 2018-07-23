d3ac98675e57d893f7cb27c0a1a8d05fd940bb23
David B. Dahl <dahl@stat.byu.edu>
2018-07-23 08:04:10 -0600

    More distributions

--- R/transcompile.R
+++ R/transcompile.R
@@ -4,7 +4,11 @@ rscalaTranscompileSubstitute <- function(x) {
   if ( x %in% c("gamma","lgamma","beta","lbeta","factorial","lfactorial","choose","lchoose","stirlingS2","sample",
                 "dnorm","pnorm","qnorm","rnorm",
                 "dgamma","pgamma","qgamma","rgamma",
+                "dexp","pexp","qexp","rexp",
                 "dbeta","pbeta","qbeta","rbeta",
+                "dt","pt","qt","rt",
+                "dchisq","pchisq","qchisq","rchisq",
+                "df","pf","qf","rf",
                 NULL) ) paste0("_",x)
   else NULL
 }
