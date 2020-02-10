b74d0162b1581cdd84c4a4d98dd8b9ce5c296aaf
David B. Dahl <dahl@stat.byu.edu>
2020-02-10 08:38:44 -0700

    Version 1.2.5

--- R/commonsMath/tests/testthat/setup-instance.R
+++ R/commonsMath/tests/testthat/setup-instance.R
@@ -1,8 +1,13 @@
 skipall <- if (requireNamespace("rscala", quietly = TRUE)) {
-  cat("Starting instance. ********************************\n")
-  library(rscala)
-  s <- scala("commonsMath")
-  FALSE
+  if ( ! isTRUE(tryCatch({rscala::scalaConfig(reconfig="offline")}, error=function(e) TRUE)) ) {
+    cat("Starting instance. ********************************\n")
+    library(rscala)
+    s <- scala("commonsMath")
+    FALSE
+  } else {
+    cat("Scala does not seem to be available. ********************************\n")
+    TRUE
+  }
 } else {
   cat("rscala is not installed. ********************************\n")
   TRUE
